50c35c48a7998fddcc9ee553ddc96f2935414356
Anatoli Makarevich <makaroni4@gmail.com>
2017-01-17 21:36:28 +0100

    Added cross validation check for best C and sigma params for SVM

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,18 +15,34 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+C_vec = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_vec = [0.01 0.03 0.1 0.3 1 3 10 30];
+err = 100 ^ 100;
 
+for i = 1:length(C_vec)
+  for j = 1:length(sigma_vec)
+    current_C = C_vec(i);
+    current_sigma = sigma_vec(j);
 
+    model = svmTrain(X, y, current_C, @(x1, x2) gaussianKernel(x1, x2, current_sigma));
+    predictions = svmPredict(model, Xval);
+    current_err = mean(double(predictions ~= yval));
 
-
+    if current_err < err
+      C = current_C;
+      sigma = current_sigma;
+      err = current_err
+    end
+  end
+end
 
 
 % =========================================================================
