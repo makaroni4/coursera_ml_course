083a9ac9e15bb3b42684b9a1d175206bda7fb4ab
Anatoli Makarevich <makaroni4@gmail.com>
2017-01-26 22:27:57 +0100

    Added cost function gradient calculation for collaborative filtering

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -42,7 +42,8 @@ Theta_grad = zeros(size(Theta));
 
 J = sum(((X * Theta' .- Y) .^ 2 .* R)(:)) / 2;
 
-
+X_grad = ((X * Theta' .- Y) .* R) * Theta;
+Theta_grad = ((X * Theta' .- Y) .* R)' * X;
 
 
 
